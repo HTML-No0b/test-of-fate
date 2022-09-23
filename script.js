4f8fd666f7da344fb1a67ddc3fa000d0234dc11e
Richard Martin <richie@Richards-MacBook-Air.local>
2022-09-23 10:30:41 -0500

    object updates

---
+++ script.js
@@ -0,0 +1,51 @@
+var questions = [
+    {
+    question: "what is html", answers: "Html is the skeleton of the website", choices: ["skin", "clothes", "hyper-markdown-language"]
+},
+{
+    question: "what is CSS", answers: "Html is the skeleton of the website", choices: ["skin", "clothes", "hyper-markdown-language", "Html is the skeleton of the website"]
+
+},
+{
+    question: "what is a DOM", answers: "Html is the skeleton of the website", choices: ["skin", "clothes", "hyper-markdown-language"]
+},
+{
+    question: "what is JavaScript", answers: "Html is the skeleton of the website", choices: ["skin", "clothes", "hyper-markdown-language"]
+
+},
+];
+// var countDown = new Date()
+
+// var x = setInterval(function) {
+//     var now = new Date().getTime(  )
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(questions[0].choices)
+question.addlistenevent("click");
