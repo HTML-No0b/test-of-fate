1fe52b25706eb489d50fe23e0b3216e3a263d284
Richard Martin <richie@Richards-MacBook-Air.local>
2022-09-23 22:37:05 -0500

    updated Objects

---
+++ assets/script.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+    question: "what is html", answers: "Html is the skeleton of the website", choices: ["skin", "clothes", "hyper-markdown-language"]
+},
+{
+    question: "what is CSS", answers: "Adds creative look to a website", choices: ["skin", "clothes", "hyper-markdown-language", "Adds creative look to a website"]
+
+},
+{
+    question: "what is a DOM", answers: "Document Object Model", choices: ["skin", "clothes", "hyper-markdown-language","Document Object Model"]
+},
+{
+    question: "what is JavaScript", answers: "", choices: ["skin", "clothes", "hyper-markdown-language"]
+
+},
+];
+const questions = [
+    {
+        
+    }
+]
+
+questions.addeventlistner("click", questions)
+console.log(questions)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(questions[0].choices)
+question.addlistenevent("click");
