b7d7144e6b7990ec2d8c8e6ca917d609b3a077a6
Richard Martin <richie@Richards-MacBook-Air.local>
2022-09-26 23:44:43 -0500

    tried adding code to subtract time

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # test-of-fate
-Code quiz
+
+## This is a simple quiz that moves on when you answer right and is supposed to add time when the answer is wrong.
+
+-   The motivation that caused me to build this website was thinking about ways I would use this in the future or how I could simply build a quiz from scratch.
+
+-   I built this project to demonstrate my ability to craft a homemade quiz
+
+-   What I learned from this project is that there are "many ways to cut a kiwi" -Nate, in the sense that there are multiple ways to have a process
+## Usage
+-   The user is supposed to click on the start quiz button, within the given time answer the correct questions and proceed to the next question. 
+    after the given time the quiz is supposed to display highscores and log them
+##  Credits
+-   https://getbootstrap.com/docs/4.5/components/modal/
--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,38 @@
 const questions = [
     {
-        question: "what is html", answers: "Html is the skeleton of the website", choices: ["skin", "clothes", "hyper-markdown-language", "Html is the skeleton of the website"]
+        question: "What is html", answers: "Html is the skeleton of the website", choices: ["skin", "clothes", "Html is the skeleton of the website", "hyper-markdown-language"]
     },
     {
-        question: "what is CSS", answers: "Adds creative look to a website", choices: ["Water", "ketchup", "hyper-markdown-language", "Adds creative look to a website"]
+        question: "What is CSS", answers: "Adds creative look to a website", choices: ["Water", "ketchup", "hyper-markdown-language", "Adds creative look to a website"]
 
     },
     {
-        question: "what is a DOM", answers: "Document Object Model", choices: ["Potatoe", "hair", "hyper-markdown-language", "Document Object Model"]
+        question: "What is a DOM", answers: "Document Object Model", choices: ["Potatoe", "hair", "Document Object Model", "hyper-markdown-language"]
     },
     {
-        question: "what is JavaScript", answers: "Javascript bring the page to Life", choices: ["waterfall", "drones", "hyper-markdown-language", "Javascript bring the page to Life"]
+        question: "What is JavaScript", answers: "Javascript bring the page to Life", choices: ["Javascript bring the page to Life", "waterfall", "drones", "hyper-markdown-language"]
 
-    },
+    }
 
 ];
 let time = 30;
 var startQuiz = document.getElementById("start-quiz");
-var quizOver = false;
+// var quizOver = false;
 var timerWorks
 var timeShow = document.getElementById("time")
+
 
 let questionPos = 0;
 startQuiz.addEventListener("click", function () {
     loadQuestions(questions[0]);
     timerWorks = setInterval(clock, 1000)
-    timeShow.textContent = time 
+    timeShow.textContent = time
 });
 function loadQuestions(x) {
     for (let i = 0; i < x.choices.length; i++) {
         document.getElementById("choice" + i).innerHTML = x.choices[i]
         document.getElementById("choice" + i).addEventListener("click", function () { checkAnswer(i) })
-        timeShow.textContent = time 
+        timeShow.textContent = time
     };
     document.getElementById("?").innerHTML = x.question
 };
@@ -43,20 +44,25 @@ function checkAnswer(answer) {
         loadQuestions(questions[questionPos])
         console.log("correct")
     }
-    else (questionPos)
-
-
-    console.log(answer)
+    else {
+        questionPos === "false" + "Wrong";
+        time - 4;
+        console.log("false");
+    }
 };
-
-
+function quizEnd() {
+    setTimeout
+    clearInterval(timerWorks)
+};
+//add endgame function
 function clock() {
     time--
-    timeShow.textContent = time 
-    //add endgame function
-    if (time === 0) {
-        
+    timeShow.textContent = time;
+
+    if (time <= 0) {
+        quizEnd();
     }
+
 };
 
 
